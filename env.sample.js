19ef0571db362840e4a7809595e6b9b87e7201fe
Anand Chowdhary <anandchowdhary@gmail.com>
2018-10-04 12:13:12 +0200

    Use process.env

--- env.sample.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	IP_INFO: "Access token",
-	GOOGLE_CLOUD: "API key",
-	AWS_ACCESS_KEY_ID: "Access key",
-	AWS_SECRET_ACCESS_KEY: "Secret key",
-	REDIS_URL: "Redis server URL"
-};
--- wrappers/ip.js
+++ wrappers/ip.js
@@ -1,10 +1,9 @@
 const axios = require("axios");
-const env = require("../env");
 
 const ip = params =>
 	new Promise((resolve, reject) => {
 		axios
-			.get(`http://ipinfo.io/${params.ip}/json?token=${env.IP_INFO}`)
+			.get(`http://ipinfo.io/${params.ip}/json?token=${process.env.IP_INFO}`)
 			.then(response => {
 				resolve(response.data);
 			})
@@ -15,7 +14,7 @@ const ip = params =>
 
 module.exports.promise = ip;
 module.exports.responder = (req, res) => {
-	const client = require("redis").createClient(env.REDIS_URL);
+	const client = require("redis").createClient(process.env.REDIS_URL);
 	client.on("error", error => new Error(error));
 	const key = `wrappers/ip/${req.params.ip}`;
 	client.get(key, (error, reply) => {
