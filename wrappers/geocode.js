a5c237a16479466698ffd6e5e5c0d0e1a8b03671
Anand Chowdhary <anandchowdhary@gmail.com>
2018-10-04 11:14:13 +0200

    Add Redis caching

--- wrappers/geocode.js
+++ wrappers/geocode.js
@@ -1,14 +1,26 @@
 const axios = require("axios");
+const client = require("redis").createClient(process.env.REDIS_URL);
+client.on("error", error => new Error(error));
 
 module.exports = (req, res) => {
-	axios
-		.get(
-			`https://maps.googleapis.com/maps/api/geocode/json?latlng=${req.params.lat},${
-				req.params.lng
-			}&key=${process.env.GOOGLE_CLOUD}`
-		)
-		.then(response => res.json(response.data))
-		.catch(error => {
-			res.json({ success: false, error: error.response.data.error });
-		});
+	const key = `wrappers/geocode/${req.params.lat}/${req.params.lng}`;
+	client.get(key, (error, reply) => {
+		if (reply) {
+			return res.json(JSON.parse(reply));
+		} else {
+			axios
+				.get(
+					`https://maps.googleapis.com/maps/api/geocode/json?latlng=${req.params.lat},${
+						req.params.lng
+					}&key=${process.env.GOOGLE_CLOUD}`
+				)
+				.then(response => {
+					res.json(response.data);
+					client.set(key, JSON.stringify(response.data));
+				})
+				.catch(error => {
+					res.json({ success: false, error: error.response.data.error });
+				});
+		}
+	});
 };
--- wrappers/ip.js
+++ wrappers/ip.js
@@ -1,10 +1,22 @@
 const axios = require("axios");
+const client = require("redis").createClient(process.env.REDIS_URL);
+client.on("error", error => new Error(error));
 
 module.exports = (req, res) => {
-	axios
-		.get(`http://ipinfo.io/${req.params.ip}/json?token=${process.env.IP_INFO}`)
-		.then(response => res.json(response.data))
-		.catch(error => {
-			res.json({ success: false, error: error.response.data.error });
-		});
+	const key = `wrappers/ip/${req.params.lat}/${req.params.lng}`;
+	client.get(key, (error, reply) => {
+		if (reply) {
+			return res.json(JSON.parse(reply));
+		} else {
+			axios
+				.get(`http://ipinfo.io/${req.params.ip}/json?token=${process.env.IP_INFO}`)
+				.then(response => {
+					res.json(response.data);
+					client.set(key, JSON.stringify(response.data));
+				})
+				.catch(error => {
+					res.json({ success: false, error: error.response.data.error });
+				});
+		}
+	});
 };
