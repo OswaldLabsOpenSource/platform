22beb05350c189ea492f85d3b64f18e45756411b
Anand Chowdhary <anandchowdhary@gmail.com>
2018-10-09 20:04:38 +0200

    Add bodyparser limit

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const ua = require("universal-analytics");
+const bodyParser = require("body-parser");
 const rateLimit = require("express-rate-limit");
 const slowDown = require("express-slow-down");
 
 const app = express();
+app.use(bodyParser.json({ limit: "50mb" }));
 app.use(express.json());
 app.use(cors());
 app.enable("trust proxy");
