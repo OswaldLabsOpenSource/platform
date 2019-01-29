f1169cd27279ac26269f93a65d35065b69d1fa1d
Anand Chowdhary <mail@anandchowdhary.com>
2019-01-29 14:54:36 +0100

    Start v2

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const app = express();
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(cors());
 app.enable("trust proxy");
-process.env.USER !== "anandchowdhary" && app.use(ua.middleware("UA-79176349-10", { cookieName: "oswald_labs_platform" }));
 
 const limiter = new rateLimit({
 	windowMs: 60000,
@@ -32,38 +31,6 @@ app.get("/", (req, res) => {
 		info: ["https://github.com/OswaldLabsOpenSource/platform", "https://oswaldlabs.com/platform"]
 	});
 });
-
-const cached = require("./cached");
-app.get("/v1/screenshot/:url", (req, res) => cached(req, res, require("./wrappers/screenshot")));
-
-// JSON
-app.use(express.json());
-
-// Public endpoints
-app.get("/v1/ip/:ip", (req, res) => cached(req, res, require("./wrappers/ip")));
-app.get("/v1/reader/:url", (req, res) => cached(req, res, require("./wrappers/reader")));
-app.post("/v1/objects", (req, res) => cached(req, res, require("./wrappers/objects")));
-app.get("/v1/geocode/:lat/:lng", (req, res) => cached(req, res, require("./wrappers/geocode")));
-app.get("/v1/weather/:lat/:lng", (req, res) => cached(req, res, require("./wrappers/weather")));
-app.get("/v1/translate/:to/:q", (req, res) => cached(req, res, require("./wrappers/translate")));
-app.get("/v1/image/:q", (req, res) => cached(req, res, require("./wrappers/image")));
-app.post("/v1/describe", (req, res) => cached(req, res, require("./wrappers/describe")));
-app.get("/v1/profile-picture/:email", (req, res) => cached(req, res, require("./wrappers/profilePicture")));
-
-// Secured endpoints
-const secure = require("./secure");
-app.post("/secure/generate", (req, res) => secure.generate(req, res));
-app.get("/secure/ip/:ip", (req, res) => secure.respond(req, res, require("./wrappers/ip")));
-app.get("/secure/reader/:url", (req, res) => secure.respond(req, res, require("./wrappers/reader")));
-app.post("/secure/objects", (req, res) => secure.respond(req, res, require("./wrappers/objects")));
-app.get("/secure/geocode/:lat/:lng", (req, res) => secure.respond(req, res, require("./wrappers/geocode")));
-app.get("/secure/weather/:lat/:lng", (req, res) => secure.respond(req, res, require("./wrappers/weather")));
-app.get("/secure/translate/:to/:q", (req, res) => secure.respond(req, res, require("./wrappers/translate")));
-app.get("/secure/image/:q", (req, res) => secure.respond(req, res, require("./wrappers/image")));
-app.post("/secure/describe", (req, res) => secure.respond(req, res, require("./wrappers/describe")));
-app.get("/secure/screenshot/:q", (req, res) => secure.respond(req, res, require("./wrappers/screenshot")));
-app.get("/secure/profile-picture/:email", (req, res) => secure.respond(req, res, require("./wrappers/profilePicture")));
-app.get("/secure/sentiments/:text", (req, res) => secure.respond(req, res, require("./wrappers/sentiments")));
 
 app.all("*", (req, res) => {
 	res.status(404).json({ error: "route not found" });
