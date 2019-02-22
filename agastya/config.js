451e8729447cc928273af05047c88474808e2255
Anand Chowdhary <mail@anandchowdhary.com>
2019-02-22 10:22:10 +0100

    Add list of your API keys

--- agastya/config.js
+++ agastya/config.js
@@ -12,3 +12,20 @@ module.exports.read = (req, res) => {
 		.then(config => res.json(config))
 		.catch(() => res.status(404).json({ error: "no_config" }));
 };
+
+module.exports.list = (req, res) => {
+	database
+		.readAll()
+		.then(list => {
+			const apiKeys = {};
+			Object.keys(list).forEach(key => {
+				if (list.hasOwnProperty(key)) if (list[key].owner == 1) apiKeys[key] = list[key];
+			});
+			return apiKeys;
+		})
+		.then(config => res.json(config))
+		.catch(error => {
+			console.log(error);
+			res.status(404).json({ error: "no_api_keys" });
+		});
+};
