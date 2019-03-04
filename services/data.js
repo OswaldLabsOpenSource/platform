bdaec981aef8290827d7ba4170ab6a5a114d6a16
Anand Chowdhary <mail@anandchowdhary.com>
2019-03-04 16:14:28 +0100

    Add Shield.io schema

--- services/data.js
+++ services/data.js
@@ -2,5 +2,5 @@ const data = require("../data.json");
 const package = require("../package.json");
 
 module.exports = (req, res) => {
-	res.json({ ...data, version: package.version });
+	res.json({ ...data, version: package.version, schemaVersion: 1, label: "month", message: parseInt(data.eventsThisMonth || 0).toLocaleString() });
 };
