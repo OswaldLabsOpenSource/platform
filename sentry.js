0804c1d5dc2870eaabe105cb995524d309e52353
Nishant Gadihoke <ngadihoke8@gmail.com>
2019-04-05 15:25:43 +0530

    Fix Sentry environment

--- sentry.js
+++ sentry.js
@@ -3,8 +3,7 @@ const constants = require("./constants");
 
 module.exports.init = () => {
 	if (constants.environment === "production")
-		Sentry.init({ dsn: "" });
-		// https://958692a6eb8a409abbd72fd22298c03c@sentry.io/1254255
+		Sentry.init({ dsn: "https://958692a6eb8a409abbd72fd22298c03c@sentry.io/1254255" });
 };
 
 module.exports.captureException = exception => {
