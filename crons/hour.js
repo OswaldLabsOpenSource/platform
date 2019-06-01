7876f2301dc76bc3c91b95e03dedb1242b8eaa24
Anand Chowdhary <mail@anandchowdhary.com>
2019-06-01 17:56:00 +0200

    :fire: Remove files

--- crons/hour.js
+++
@@ -1,3 +0,0 @@
-module.exports = () => {
-	require("./processes/update-quota")();
-};
--- update.sh
+++
@@ -1,8 +0,0 @@
-git pull
-yarn
-pm2 restart platform
-cd agastya-database
-git add .
-git commit -m "Agastya database backup"
-git push origin master --force
-cd ../
