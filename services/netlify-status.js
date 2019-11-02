9ba0d9a8d8d0cd843ecfd5a7bd994cc7532b5c77
Anand Chowdhary <mail@anandchowdhary.com>
2019-11-02 13:37:26 -0700

    Add update notice to Netlify badge

--- services/netlify-status.js
+++ services/netlify-status.js
@@ -1,36 +1,8 @@
-const axios = require("axios");
-const sentry = require("../sentry");
-sentry.init();
-
 module.exports = (req, res) => {
-	const netlifyUrl = `https://api.netlify.com/api/v1/badges/${req.params.key}/deploy-status`;
-	axios
-		.get(netlifyUrl)
-		.then(response => {
-			const badge = response.data;
-			let status = "error";
-			let color = "critical";
-			if (badge.includes("#007A70")) {
-				status = "success";
-				color = "success";
-			}
-			if (badge.includes("#A88100")) {
-				status = "building";
-				color = "yellow";
-			}
-			res.json({
-				schemaVersion: 1,
-				label: "netlify",
-				message: status,
-				color
-			});
-		})
-		.catch(() => {
-			res.json({
-				schemaVersion: 1,
-				label: "netlify",
-				message: "not found",
-				color: "critical"
-			});
-		});
+	res.json({
+		schemaVersion: 1,
+		label: "netlify",
+		message: "update this badge (badges/shields#3129)",
+		color: "critical"
+	});
 };
